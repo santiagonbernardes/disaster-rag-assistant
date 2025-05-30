d32f2c1b8a923818f07564b29c0ab8f1be0d3361
Santiago Bernardes <santiagobernardes@gmail.com>
2025-05-30 13:49:16 -0300

    stores as session in langfuse

--- app.py
+++ app.py
@@ -1,7 +1,27 @@
+import tomllib
+from pathlib import Path
+
 from src.ui.chatbot import main
+from langfuse.decorators import langfuse_context
+import streamlit as st  # Using ST to manage the secrets for now
+
+
+def get_app_version():
+    pyproject_path = Path(__file__).parent / "pyproject.toml"
+    with open(pyproject_path, "rb") as f:
+        pyproject_data = tomllib.load(f)
+    return pyproject_data["project"]["version"]
+
 
 if __name__ == "__main__":
     # This file is the entrypoint of the application.
     # Add important configuration here like integrations,
     # logging, etc.
+
+    langfuse_context.configure(
+        secret_key=st.secrets["langfuse_secret_key"],
+        public_key=st.secrets["langfuse_public_key"],
+        host=st.secrets["langfuse_host"],
+        environment=st.secrets["langfuse_environment"],
+    )
     main()
--- src/ui/chatbot.py
+++ src/ui/chatbot.py
@@ -1,17 +1,20 @@
 import streamlit as st
-from openai import OpenAI
+from langfuse.openai import OpenAI
+from langfuse.decorators import observe, langfuse_context
+import uuid
 
 
-def get_openai_client():
+@st.cache_resource
+def client():
     return OpenAI(api_key=st.secrets["openai_api_key"])
 
 
-def render_chat_history(client):
+def render_chat_history():
     previous_response_id = st.session_state.previous_response_id
     if previous_response_id is None:
         return
 
-    all_messages = client.responses.input_items.list(
+    all_messages = client().responses.input_items.list(
         previous_response_id, order="asc"
     ).data
 
@@ -22,28 +25,40 @@ def render_chat_history(client):
     with st.chat_message("assistant"):
         st.markdown(st.session_state.last_response)
 
+@observe
+def get_an_response(user_prompt):
+    langfuse_context.update_current_observation(session_id=st.session_state.session_id)
+
+    return client().responses.create(
+        model="gpt-4.1-nano",
+        input=user_prompt,
+        instructions="Você é um assistente simpático e suscinto.",
+        previous_response_id=st.session_state.previous_response_id,
+    )
+
 
 def main():
     st.title("Simple bot")
-    client = get_openai_client()
-
     if "previous_response_id" not in st.session_state:
         st.session_state.previous_response_id = None
     if "last_response" not in st.session_state:
         st.session_state.last_response = None
+    if "session_id" not in st.session_state:
+        st.session_state.session_id = uuid.uuid4().hex
+    if "user_profile" not in st.session_state:
+        st.session_state.user_profile = None
 
-    render_chat_history(client)
+    # if not st.session_state.user_profile:
+    #     st.session_state.user_profile = prompt_user_for_profile()
+    #     return
+
+    render_chat_history()
 
     if prompt := st.chat_input("Type a message"):
         with st.chat_message("user"):
             st.markdown(prompt)
 
-        response = client.responses.create(
-            model="gpt-4.1-nano",
-            input=prompt,
-            instructions="ALWAYS RETURN 'Não sei', regardless of the input.",
-            previous_response_id=st.session_state.previous_response_id,
-        )
+        response = get_an_response(prompt)
 
         st.session_state.last_response = response.output_text
         st.session_state.previous_response_id = response.id

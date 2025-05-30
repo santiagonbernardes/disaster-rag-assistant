160f0d9886bd6a0a2339b303f76774ac1334e719
Santiago Bernardes <santiagobernardes@gmail.com>
2025-05-30 11:16:54 -0300

    streamlit works

---
+++ app.py
@@ -0,0 +1,7 @@
+from src.ui.chatbot import main
+
+if __name__ == "__main__":
+    # This file is the entrypoint of the application.
+    # Add important configuration here like integrations,
+    # logging, etc.
+    main()
---
+++ src/ui/chatbot.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+
+def main():
+    st.write("Hello from disaster-rag-assistant!")
